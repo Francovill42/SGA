8b2f71d456b9e54f764e63c245e81f5c0843fc9e
Francovill42 <fvillan500@gmail.com>
2026-08-31 15:39:15 -0300

    Clase 14 aplicamos nuevos metodos POST,PUT,GET, etc..

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 const express = require("express")
 
 const app = express()
+app.use(express.json())
+
 
 const alumnos = [
     {
@@ -48,6 +50,24 @@ app.get("/alumnos/:id", (req, res) => {
     res.json(alumno) 
 }),
 
+app.post("/alumnos",(req, res) => {
+    const nuevoAlumno = req.body
+    alumnos.push(nuevoAlumno)
+
+    res.json({mensaje: "Alumno registrado correctamente"})
+})
+
+app.put("/alumnos/:id", (req, res) => {
+    const id = Number(req.params.id)
+    const alumno = alumnos.find(alumno => alumno.id === id)
+    alumno.id = req.body.id
+    alumno.nombre = req.body.nombre
+    alumno.carrera = req.body.carrera
+    res.json({mensaje: "Alumno actualizado correctamente mi REYYYYYYYYYYYYYYY"})
+})
+
+
 app.listen(3000, () => {
     console.log("Servidor funcionando en http://localhost:3000")
 })
+
